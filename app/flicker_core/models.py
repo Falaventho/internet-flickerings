4981c879c82a12d328b14d4ae79383fe441f3465
Falaventho <falaventho@gmail.com>
2024-07-31 10:06:10 -0400

    implemented basic data models for core functionality

--- app/flicker_core/models.py
+++ app/flicker_core/models.py
@@ -7,3 +7,67 @@ class FlickerUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class MediaRating(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Show(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Season(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    parent_show = models.ForeignKey(Show, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class MediaObject(models.Model):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    thumbnail_url = models.URLField()
+    release_year = models.IntegerField()
+    runtime = models.IntegerField()
+    rating = models.ForeignKey(MediaRating, on_delete=models.PROTECT)
+    season = models.ForeignKey(Season, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class GenreMediaLink(models.Model):
+    media = models.ForeignKey(MediaObject, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+
+class CastCrewMember(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Credited(models.Model):
+    media = models.ForeignKey(MediaObject, on_delete=CASCADE)
+    credited_member = models.ForeignKey(CastCrewMember, on_delete=CASCADE)
