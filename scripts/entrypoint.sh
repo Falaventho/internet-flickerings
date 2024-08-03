daf9d822f7ee9439a422ccddb99bb9a4b880532a
Falaventho <falaventho@gmail.com>
2024-08-03 16:10:40 -0400

    altered entrypoint to check for test flag again

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+
+if ["$TEST_FLAG" == "true" ]; then
+    echo "Running tests"
+    python manage.py test
+else
+    echo "Test flag set to '$TEST_FLAG', skipping tests"
+fi
+
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-echo "Running tests"
-python manage.py test
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
