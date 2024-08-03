41ee5140c81a88a17ccd882896b0c6f8fc135541
Falaventho <falaventho@gmail.com>
2024-08-03 15:30:01 -0400

    tweaked entrypoint to make tests run

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,9 +5,8 @@ set -e
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-
 echo "TEST_FLAG set to '$TEST_FLAG'"
-if [ "$TEST_FLAG" == "true"]
+if [ "$TEST_FLAG" = "true"]
 then
     echo "Running tests"
     python manage.py test
