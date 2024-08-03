2a70e3c071e69c506ea7f9ec92924f78568a8fc9
Falaventho <falaventho@gmail.com>
2024-08-03 15:26:32 -0400

    altered test flag to operate consistently, resolving previous ci issues or revealing the next layer

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,8 +6,8 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-echo "TEST variable set to '$TEST'"
-if [ "$TEST" == "1"]
+echo "TEST_FLAG set to '$TEST_FLAG'"
+if [ "$TEST_FLAG" == "true"]
 then
     echo "Running tests"
     python manage.py test
