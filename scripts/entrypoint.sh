5ea617a6181a0cd0d8789d027f074c585375e81b
Falaventho <falaventho@gmail.com>
2024-08-03 16:12:30 -0400

    altered order of commands for organization

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-
-if ["$TEST_FLAG" == "true" ]; then
-    echo "Running tests"
-    python manage.py test
-else
-    echo "Test flag set to '$TEST_FLAG', skipping tests"
-fi
-
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "TEST_FLAG variable set to '$TEST_FLAG'"
+if [ "$TEST_FLAG" == "true" ]
+then
+    echo "Running tests"
+    python manage.py test
+else
+    echo "Test flag set to false or unset, tests skipped"
+fi
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
