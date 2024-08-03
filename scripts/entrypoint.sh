4a46457c14fde708b44c1d093e6bb6f2fb227dc5
Falaventho <falaventho@gmail.com>
2024-08-03 15:54:49 -0400

    temporary bypass of test check for runner config

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,13 +5,7 @@ set -e
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-echo "TEST_FLAG set to '$TEST_FLAG'"
-if [ "$TEST_FLAG" == "true" ]
-then
-    echo "Running tests"
-    python manage.py test
-else
-    echo "Test flag set to false or unset, tests skipped"
-fi
+echo "Running tests"
+python manage.py test
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
