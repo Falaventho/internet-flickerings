72a8049af559e550135381716a7c84de4bba77e5
Falaventho <falaventho@gmail.com>
2024-08-03 09:44:19 -0400

    added gitlab ci and configured build and test stages

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,13 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
+
+if [ "$TEST" == "true"]
+then
+    echo "Running tests"
+    python manage.py test
+else
+    echo "Tests skipped"
+fi
+
 python manage.py runserver 0.0.0.0:8000
