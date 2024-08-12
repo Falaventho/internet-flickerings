a39ca5761dfa14ec20856458de363003180a4125
Falaventho <falaventho@gmail.com>
2024-08-12 17:57:32 -0400

    added session-based 'recently viewed' section on browse page

--- app/flicker_core/views.py
+++ app/flicker_core/views.py
@@ -8,11 +8,17 @@ def home(request):
 
 
 def browse(request):
-    cards = MediaObject.objects.all()
+    example_cards = MediaObject.objects.all()
+    recent_ids = request.session.get('recents', [])
+    recent_cards = MediaObject.objects.filter(id__in=recent_ids)
     sections = [
         {
             "title": "Example Section",
-            "cards": cards
+            "cards": example_cards,
+        },
+        {
+            "title": "Recently Viewed",
+            "cards": recent_cards
         }
     ]
     return render(request, 'flicker_core/browse.html', {'sections': sections})
@@ -20,6 +26,13 @@ def browse(request):
 
 def watch(request, media_id):
     media_object = MediaObject.objects.get(id=media_id)
+
+    recently_viewed = request.session.get('recents', [])
+
+    if media_id not in recently_viewed:
+        recently_viewed.append(media_id)
+        request.session['recents'] = recently_viewed
+
     return render(request, 'flicker_core/watch.html', {
         'title': media_object.title,
         'content_uri': media_object.content_uri,
